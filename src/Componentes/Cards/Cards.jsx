70fbb6c1a1ee136a94c387d37e3d7210ecfaaf10
Adrian <adrian>
2024-05-08 13:00:07 +0200

    navbar responsive

--- src/Componentes/Cards/Cards.jsx
+++ src/Componentes/Cards/Cards.jsx
@@ -1,21 +1,20 @@
 import { Link } from 'react-router-dom';
 import { useCarrito } from '../../usecontext/CarritoContext';
 import { FaCartShopping } from "react-icons/fa6";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Cards.css';
 
-const Cards = ({ products}) => {
-  const { añadir, mensaje } = useCarrito(); 
-  const [productosOrdenados, setProductosOrdenados] = useState(products); 
+const Cards = ({ products }) => {
+  const { añadir } = useCarrito(); 
+  const [productosOrdenados, setProductosOrdenados] = useState([]); 
   const [orden, setOrden] = useState('nombreAsc'); 
-  const [productoAñadido, setProductoAñadido] = useState(null);
 
-  const handleClick = (id) => {
-    setProductoAñadido(id);
-  };
+  useEffect(() => {
+    ordenarProductos(orden); // Ordenar productos al renderizar
+  }, [orden, products]); // Ejecutar cuando cambie la orden o los productos
+
   const handleChangeOrden = (e) => {
     setOrden(e.target.value);
-    ordenarProductos(e.target.value);
   };
 
   const ordenarProductos = (tipoOrden) => {
@@ -31,63 +30,55 @@ const Cards = ({ products}) => {
     }
     setProductosOrdenados(sortedProducts);
   };
- if (!productosOrdenados) { 
+
+  if (!productosOrdenados) { 
     return <div>Cargando...</div>;
   }
+
   return ( 
-  <>
-  <section  className="container">
-    <div className='cards_select'>
-      <label htmlFor="orden">Ordenar por:</label>
-      <select id="orden" value={orden} onChange={handleChangeOrden}>
-        <option value="nombreAsc">Nombre (A-Z)</option>
-        <option value="nombreDesc">Nombre (Z-A)</option>
-        <option value="precioAsc">Precio (Menor a Mayor)</option>
-        <option value="precioDesc">Precio (Mayor a Menor)</option>
-      </select>
-    </div>
-      <div className="container_cards">
-        {productosOrdenados.map((product) => (
-          <div className="card" key={product._id}>
-            <div>
-              <Link to={`/product/${product.nombre}`}>
-                <img src={product.imagen} alt={product.nombre} width='300px'/>
-              </Link>
-            </div>
-            <div className="text_card">
-              <h3>
+    <>
+      <section className="container">
+        <div className='cards_select'>
+          <label htmlFor="orden">Ordenar por:</label>
+          <select id="orden" value={orden} onChange={handleChangeOrden}>
+            <option value="nombreAsc">Nombre (A-Z)</option>
+            <option value="nombreDesc">Nombre (Z-A)</option>
+            <option value="precioAsc">Precio (Menor a Mayor)</option>
+            <option value="precioDesc">Precio (Mayor a Menor)</option>
+          </select>
+        </div>
+        <div className="container_cards">
+          {productosOrdenados.map((product) => (
+            <div className="card" key={product._id}>
+              <div>
                 <Link to={`/product/${product.nombre}`}>
-                  {product.id} - {product.nombre}
+                  <img src={product.imagen} alt={product.nombre} width='300px'/>
                 </Link>
-              </h3>
-              <Link to={`/product/${product.categoria}`}>
-              <h4 className='card_categoria'>{product.categoria}</h4>
-              </Link>
-              {typeof product.precio === "number" ? (
-                <>
-                <div className="card_carro">
-                 <h4>{product.precio} €</h4>
-                      <FaCartShopping className='cart-shopping' onClick={() => {
-                        añadir(product);
-                        handleClick(product.id);
-                      }} /> 
-                      </div>
-                      <div className='mensaje'>
-                    {productoAñadido === product.id && mensaje && <p>{mensaje}</p>}
-                   
-                </div>
-                </>
-              ) : (
-                <div className='card_carro'>
-                <h4>No disponible</h4>
-                </div>
-              )}
+              </div>
+              <div className="text_card">
+                <h3>
+                  <Link to={`/product/${product.nombre}`}>
+                    {product.id} - {product.nombre}
+                  </Link>
+                </h3>
+                <Link to={`/product/${product.categoria}`}>
+                  <h4 className='card_categoria'>{product.categoria}</h4>
+                </Link>
+                {typeof product.precio === "number" ? (
+                  <div className="card_carro">
+                    <h4>{product.precio} €</h4>
+                    <FaCartShopping className='cart-shopping' onClick={() => añadir(product)} />
+                  </div>
+                ) : (
+                  <div className='card_carro'>
+                    <h4>No disponible</h4>
+                  </div>
+                )}
+              </div>
             </div>
-          </div> 
-        ))}
-      </div>
+          ))}
+        </div>
       </section>
-      ;
     </>
   );
 };
